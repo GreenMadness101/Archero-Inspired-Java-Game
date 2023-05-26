f2e137e5caf93694477f1f45467790aaf6d1a352
SAMARTH VYSYARAJU <svysyaraju25@student.gvsd.org>
2023-05-26 12:28:07 -0400

    new map yay i wanna kms

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -27,7 +27,7 @@ public class TileManager
     mapTileNum = new int[gp.getMaxScreenCol()][gp.getMaxScreenRow()];
 
     getTileImage();
-    loadMap("/res/maps/start.txt");
+    loadMap("/res/maps/map7.txt");
   }
 
   public void getTileImage()
