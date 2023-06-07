3627a04207d58ab66fc739543f3ad2923eb2a421
SAMARTH VYSYARAJU <svysyaraju25@student.gvsd.org>
2023-06-07 11:27:41 -0400

    added bat to map

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Entity;
+import monster.MON_Bat;
 import monster.MON_GreenSlime;
 
 public class AssetSetter 
@@ -13,24 +14,15 @@ public class AssetSetter
     }
     public void setMonster()
     {
-<<<<<<< HEAD
-        gp.monster[0] = new MON_GreenSlime(gp);
-        gp.monster[0].setX(200);
-        gp.monster[0].setY(200);
-        gp.monster[1] = new MON_GreenSlime(gp);
-        gp.monster[1].setX(300);
-        gp.monster[1].setY(200);
-        gp.monster[2] = new Entity(gp);
-        gp.monster[2].setX(250);
-        gp.monster[2].setY(250);
-=======
         gp.monster.add(new MON_GreenSlime(gp));
         gp.monster.get(0).setX(200);
         gp.monster.get(0).setY(200);
         gp.monster.add(new MON_GreenSlime(gp));
         gp.monster.get(1).setX(300);
         gp.monster.get(1).setY(200);
->>>>>>> 6bcb4b21c8fce77584fa67a2ccb851b3af07f197
+        gp.monster.add(new MON_Bat(gp));
+        gp.monster.get(2).setX(250);
+        gp.monster.get(2).setY(250);
 
     }
     
